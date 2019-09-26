3f8e25e10186a2483c77c9310d464f890d6e3e01
sharmara <rakesh.sharma@arhs-dev.com>
2019-09-26 19:45:29 +0200

    test 004 added

---
+++ src/test/java/test/TC004_UpdatePetDetails.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+
+import baseUtils.StartUpConfig;
+import pages.PetCreation;
+import pages.PetList;
+
+public class TC004_UpdatePetDetails {
+	
+	WebDriver driver;
+	StartUpConfig sc = new StartUpConfig();
+	
+
+}
